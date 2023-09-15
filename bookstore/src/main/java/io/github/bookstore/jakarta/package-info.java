cb4363b4f66e41043a6a57aa9f25db181ed774a5
Fernando Comunello <66500627+fercomunello@users.noreply.github.com>
2023-09-15 15:56:14 -0300

    Add package-info.java docs

---
+++ bookstore/src/main/java/io/github/bookstore/jakarta/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * <h3>The Jakarta Bookstore</h3>
+ *
+ * <p>A scalable e-commerce with back-office included system for bookstores.</p> <br>
+ *
+ * <p>This project demonstrates real knowledge of how the Jakarta EE plataform works
+ * and for what application servers are used for.</p> <br>
+ *
+ * <p>Check out the official documentation on Github, here is some links:</p>
+ * -> <a href="https://github.com/fercomunello/jakarta-bookstore">Read-me</a> <br>
+ * -> <a href="https://github.com/fercomunello/jakarta-bookstore/wiki">Project Wiki</a> <br>
+ * -> <a href="https://github.com/fercomunello/jakarta-bookstore/wiki/Home-pt-BR">Wiki do projeto (pt-BR)</a> <br>
+ *
+ * @author Fernando Comunello (@ferjava)
+ */
+package io.github.bookstore.jakarta;
